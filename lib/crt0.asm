1e59ef64ebf6e38b6ff6d6a78ebb76fc696869e4
jayvalentine <jayv136@gmail.com>
2020-08-24 17:57:18 +0100

    Removed library files from this repository; they now live in z80-libraries

--- lib/crt0.asm
+++
@@ -1,7 +0,0 @@
-    ; CRT0 for modularz80 platform.
-    ; For running code in user-memory.
-    EXTERN  _main
-
-start:
-    call    _main
-    ret
--- lib/include/midi.h
+++
@@ -1,6 +0,0 @@
-// MIDI handling library for a 76489 PSG.
-// By Jay Valentine
-
-void midi_init(unsigned char port);
-void midi_note_on(unsigned char track, unsigned char note);
-void midi_note_off(unsigned char track, unsigned char note);
